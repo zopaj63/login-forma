9309640caf56b45ad89434e483354a4b32feabef
zopaj63 <zoran.pajnic@gmail.com>
2023-10-03 22:57:17 +0200

    login - view, controller, route

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    public function showLoginForm()
+    {
+        return view('login');
+    }
+
+    public function login(Request $request)
+    {
+        $credentials=$request->only('email', 'password');
+        if(Auth::attempt($credentials))
+        {
+            // Uspješna prijava
+            return redirect()->route('home')->with('success', 'Uspješna prijava');
+        }
+        return redirect()->route('login')->with('error', 'Pogrešan email ili lozinka');
+    }
+}
---
+++ resources/views/login.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    <h1>Login forma</h1>
+    <hr>
+    <form action="{{route('login')}}" method="POST">
+        @csrf
+        <label>Email</label>
+        <input type="email" name="email" required>
+        <br><br>
+        <label>Lozinka</label>
+        <input type="password" name="password" required>
+        <br><br><br>
+        <button type="submit" value="Login">Registriraj se</button>
+    </form>
+
+</body>
+</html>
